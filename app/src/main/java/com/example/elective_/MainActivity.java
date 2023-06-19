d4cb14142dc3a3c4242dddeaad4cca44321e5385
unknown <abhishekbhande248@gmail.com>
2023-06-19 18:48:47 +0530

    first commit

---
+++ app/src/main/java/com/example/elective_/MainActivity.java
@@ -0,0 +1,225 @@
+package com.example.elective_;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.PopupMenu;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    FirebaseAuth auth;
+    FirebaseFirestore firestore;
+    //For Current user
+//    FirebaseUser user=auth.getCurrentUser();
+
+
+    String email,password;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+
+        Button gettingstarted=(Button)findViewById(R.id.getting_started);
+        Button student=(Button)findViewById(R.id.student);
+
+
+
+//        Button logout=(Button) findViewById(R.id.logout);
+
+        firestore=FirebaseFirestore.getInstance();
+//        auth=FirebaseAuth.getInstance();
+
+        Map<String,String> users=new HashMap<>();
+
+        users.put("fname","abhi");
+        users.put("lname","bh");
+
+//        firestore.collection("users").add(users).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+//            @Override
+//            public void onSuccess(DocumentReference documentReference) {
+//                Toast.makeText(MainActivity.this, "Success", Toast.LENGTH_SHORT).show();
+//            }
+//        }).addOnFailureListener(new OnFailureListener() {
+//            @Override
+//            public void onFailure(@NonNull Exception e) {
+//                Toast.makeText(MainActivity.this, "Failuer", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+
+
+        //setting name and email after login
+
+//        if(user != null)
+//        {
+//            name.setText(user.getDisplayName());
+//            email.setText(user.getEmail());
+//        }
+
+
+        //logout
+
+//        logout.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                auth.signOut();
+//                startActivity(new Intent(MainActivity.this,loginactivity.class));
+//        finish();
+//            }
+//        });
+
+
+        //Updating user details
+
+
+
+        gettingstarted.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                view.setBackgroundResource(R.drawable.rectangle_2_);
+
+                Intent i=new Intent(getApplicationContext(),student_login.class);
+                startActivity(i);
+
+                if(student.getText().equals("Student"))
+                {
+                    Toast.makeText(MainActivity.this, "Clicked", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+
+
+
+        student.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                view.setBackgroundResource(R.drawable.rectangle_4_);
+                PopupMenu popupMenu=new PopupMenu(getApplicationContext(),student);
+
+                popupMenu.getMenuInflater().inflate(R.menu.as_menu,popupMenu.getMenu());
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem menuItem) {
+                       student.setText(menuItem.getTitle());
+                       student.setBackgroundResource(R.drawable.rectangle_4);
+                       return true;
+                    }
+                });
+                popupMenu.show();
+            }
+
+        });
+
+
+    }
+
+    //User Exist or not
+
+//    @Override
+//    protected void onStart() {
+//        super.onStart();
+//
+//        if(auth.getCurrentUser() != null)
+//        {
+////            startActivity();
+//            finish();
+//        }
+//    }
+
+
+
+
+    //User is login or not
+
+//    @Override
+//    protected void onStart() {
+//        super.onStart();
+//
+//        if(user == null)
+//        {
+//            startActivity(new Intent(this,login.class));
+//            finish();
+//        }
+//    }
+
+
+//    public void createUser()
+//    {
+//        auth.createUserWithEmailAndPassword(email,password)
+//                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<AuthResult> task) {
+//                        if(task.isSuccessful())
+//                        {
+//                            Toast.makeText(MainActivity.this, "Account Created", Toast.LENGTH_SHORT).show();
+//                        }
+//                        else
+//                        {
+//                            Toast.makeText(MainActivity.this,"Error :"+task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+//                        }
+//                    }
+//                });
+//    }
+
+//    public void signIn()
+//    {
+//        auth.signInWithEmailAndPassword(email,password)
+//                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<AuthResult> task) {
+//                        if(task.isSuccessful()) {
+//                            Toast.makeText(MainActivity.this, "Login Successfully", Toast.LENGTH_SHORT).show();
+//                               // startActivity(intent);
+//                              update();
+//                        }else {
+//                            Toast.makeText(MainActivity.this, "Error :"+task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+//                        }
+//                    }
+//                });
+//    }
+
+
+    //public void update()
+//    {
+//        UserProfileChangeRequest changeRequest=new UserProfileChangeRequest.Builder()
+//                .setDisplayName(name).build();
+//
+//        auth.getCurrentUser().updateProfile(changeRequest);
+//
+//        openLogin();
+//    }
+
+
+//    public void openLogin()
+//    {
+//        startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+//        finish();
+//    }
+
+
+
+}
